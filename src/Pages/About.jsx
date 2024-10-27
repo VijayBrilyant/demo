2697b2e14fa9ae080db5a823320d04590a3e4efc
Vijaykumar S <vijaykumars@Vijaykumars-MacBook-Pro.local>
2024-10-27 08:56:17 +0530

    first commit

---
+++ src/Pages/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import { Link } from 'react-router-dom'
+import Navbar from '../Layouts/Navbar'
+
+const About = () => {
+  return (
+    <>
+    
+<Navbar navbarName="Logo About"/>
+
+<h1>Welcome to About us page</h1>
+
+
+    </>
+  )
+}
+
+export default About
